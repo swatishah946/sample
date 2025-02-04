0f195df8c3fd40220e9def4229a2d6ceb5bdf504
swatishah946 <shahswati946@gmail.com>
2025-02-04 22:44:26 +0530

    backend msme

--- sample_app/src/App.jsx
+++ sample_app/src/App.jsx
@@ -1,43 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import ForgotPassword from "./components/auth/ForgotPassword";
-import ShipmentsPage from './pages/ShipmentsPage';
-import LoadMatchingPage from './pages/LoadMatchingPage';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/auth/signup">Register</Link>
-            </li>
-            <li>
-              <Link to="/auth/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/shipments">Shipments</Link>
-            </li>
-            <li>
-              <Link to="/load-matching">Load Matching</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Navigate to="/auth/login" />} />
-          <Route path="/auth/*" element={<AuthPage />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/signup" element={<SignUp />} />
-          <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-          <Route path="/shipments" element={<ShipmentsPage />} />
-          <Route path="/load-matching" element={<LoadMatchingPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth/login" />} />
+        <Route path="/auth" element={<AuthPage />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
