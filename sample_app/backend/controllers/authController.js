1063e7489169d703efa8af27deef75984089d03e
anishka7755 <anishkaverma046@gmail.com>
2025-02-07 11:39:56 +0530

    added role authenticating

--- sample_app/backend/controllers/authController.js
+++ sample_app/backend/controllers/authController.js
@@ -33,21 +33,20 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-
+    if (user.role !== role)
+      return res.status(400).json({ message: "Role mismatch" });
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-  
     res.status(200).json({
       success: true,
       token,
       role: user.role,
-      message: "Login successful"
+      message: "Login successful",
     });
-    
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
